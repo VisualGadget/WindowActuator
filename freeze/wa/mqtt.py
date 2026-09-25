78993a930b1dddd1cca25fda24d2c557bfefb740
Visual Gadget <dm2dm@ya.ru>
2026-09-25 07:11:55 +0500

    Fix never offline

--- freeze/wa/mqtt.py
+++ freeze/wa/mqtt.py
@@ -8,26 +8,35 @@ from wa.utils import wifi_mac
 
 
 class MQTTWindowActuator:
-    # """
-    # Home Assistant MQTT window actuator device
-    # """
+    """
+    Home Assistant MQTT window actuator device.
+    """
 
     _WINDOW_DEV = 'window'
     _STALE_DETECTOR_DEV = 'stale_detector'
     _STATE_UPDATE_INTERVAL_S = 20 * 60  # 20 min
+    _MQTT_KEEPALIVE_S = 60
+    _MQTT_PING_INTERVAL_S = _MQTT_KEEPALIVE_S // 2
+    _MQTT_RECONNECT_INTERVAL_S = 10
 
     def __init__(self, server: str, port: int, user: str, password: str, servo: Servo, client_name: str):
-        # """
-        # :param server: server address
-        # :param port: server port
-        # :param user: user
-        # :param password: password
-        # :param servo: window servomotor
-        # :param client_name: MQTT client name
-        # """
+        """
+        Initialize the MQTT window actuator.
+
+        :param server: MQTT broker address.
+        :param port: MQTT broker port.
+        :param user: MQTT username.
+        :param password: MQTT password.
+        :param servo: Window servomotor.
+        :param client_name: MQTT client name.
+        """
         self._servo = servo
-        self._position: float = None
+        self._position = 0.0
         self._stalled = False
+        self._connected = False
+        self._last_ping = 0.0
+        self._last_connect_attempt = 0.0
+        self.last_update = time.time()
 
         mac = wifi_mac()
         device = {
@@ -36,11 +45,10 @@ class MQTTWindowActuator:
             'name': 'Window',
             'identifiers': mac
         }
-        self._devices = {
+        self._availability_topic = f'Household/window/{client_name}/availability'
+        self._devices: dict = {
             self._WINDOW_DEV: {
-                'device_class': 'window',
-                'unit_of_measurement': '%',
-                'expire_after': self._STATE_UPDATE_INTERVAL_S * 3
+                'device_class': 'window'
             },
             self._STALE_DETECTOR_DEV: {
                 'device_class': 'problem',
@@ -52,11 +60,12 @@ class MQTTWindowActuator:
             server=server,
             port=port,
             user=user,
-            password=password
+            password=password,
+            keepalive=self._MQTT_KEEPALIVE_S
         )
         self._mqtt.set_callback(self._inbox)
-        self._connect()
 
+        self._discovery_topics: dict = {}
         for dev_name, sensor_info in self._devices.items():
             uid = f'{client_name}_{dev_name}'
             topic_base = f'Household/window/{uid}'
@@ -64,6 +73,9 @@ class MQTTWindowActuator:
             sensor_info['name'] = dev_name
             sensor_info['unique_id'] = uid
             sensor_info['device'] = device
+            sensor_info['availability_topic'] = self._availability_topic
+            sensor_info['payload_available'] = 'online'
+            sensor_info['payload_not_available'] = 'offline'
 
             if dev_name == self._WINDOW_DEV:
                 platform = 'cover'
@@ -75,19 +87,12 @@ class MQTTWindowActuator:
                 platform = 'binary_sensor'
                 sensor_info['state_topic'] = topic_base + '/stale/notify'
 
-            # HA MQTT discovery
-            ha_discovery_topic = f'homeassistant/{platform}/{uid}/config'
-            self._mqtt.publish(ha_discovery_topic, json.dumps(sensor_info), True)
-
-            # command subscriptions
-            for set_topic in ('command_topic', 'set_position_topic'):
-                if set_topic in sensor_info:
-                    self._mqtt.subscribe(sensor_info[set_topic])
+            self._discovery_topics[dev_name] = f'homeassistant/{platform}/{uid}/config'
 
         self._retrieve_current_position()
-        self.send_update()
+        self._connect()
 
-    def _retrieve_current_position(self):
+    def _retrieve_current_position(self) -> None:
         # """
         # Current position is servo position
         # """
@@ -111,34 +116,109 @@ class MQTTWindowActuator:
         """
         self._servo.set_motor_power(power=power)
 
-    def _connect(self):
-        self._mqtt.connect()
+    def _publish_discovery(self) -> None:
+        """
+        Publish retained Home Assistant discovery data and restore subscriptions.
+        """
+        for dev_name, sensor_info in self._devices.items():
+            self._mqtt.publish(
+                self._discovery_topics[dev_name],
+                json.dumps(sensor_info),
+                True
+            )
 
-    def send_update(self):
+            for set_topic in ('command_topic', 'set_position_topic'):
+                if set_topic in sensor_info:
+                    self._mqtt.subscribe(sensor_info[set_topic])
+
+    def _mark_disconnected(self, error: Exception | None = None) -> None:
+        """
+        Close a failed MQTT connection and schedule another connection attempt.
+
+        :param error: Error that caused the connection to fail.
+        """
+        if error:
+            print(f'MQTT connection lost: {error}')
+
+        self._connected = False
+        sock = self._mqtt.sock
+        if sock is not None:
+            try:
+                sock.close()
+            except Exception:
+                pass
+
+        self._mqtt.sock = None
+
+        self._last_connect_attempt = time.time()
+
+    def _connect(self) -> None:
+        """
+        Connect to the broker, publish availability, and initialize MQTT state.
+        """
+        self._last_connect_attempt = time.time()
+        try:
+            self._mqtt.set_last_will(
+                self._availability_topic,
+                'offline',
+                retain=True
+            )
+            self._mqtt.connect(timeout=5)
+            self._connected = True
+            self._publish_discovery()
+            self._mqtt.publish(self._availability_topic, 'online', True)
+            self._last_ping = time.time()
+            self.send_update()
+            if self._connected:
+                print('Connected to MQTT broker')
+        except Exception as ex:
+            self._mark_disconnected(ex)
+
+    def send_update(self) -> None:
         # """
         # Send parameters update to MQTT server
         # """
-        state = ('OFF', 'ON')[self._stalled]
-        self._mqtt.publish(self._devices[self._STALE_DETECTOR_DEV]['state_topic'], state)
+        if not self._connected:
+            return
 
-        pos = str(self._position * 100)
-        self._mqtt.publish(self._devices[self._WINDOW_DEV]['position_topic'], pos)
+        try:
+            state = ('OFF', 'ON')[self._stalled]
+            self._mqtt.publish(self._devices[self._STALE_DETECTOR_DEV]['state_topic'], state)
+
+            pos = str(self._position * 100)
+            self._mqtt.publish(self._devices[self._WINDOW_DEV]['position_topic'], pos)
+        except Exception as ex:
+            self._mark_disconnected(ex)
+            return
 
         self.last_update = time.time()
 
     async def run(self):
-        # """
-        # Main event loop
-        # """
+        """
+        Run the MQTT and servo event loop.
+        """
 
         while True:
-            self._mqtt.check_msg()
+            if self._connected:
+                try:
+                    self._mqtt.check_msg()
+
+                    if self._connected:
+                        now = time.time()
+                        if now - self._last_ping >= self._MQTT_PING_INTERVAL_S:
+                            self._mqtt.ping()
+                            self._last_ping = now
+
+                        if now - self.last_update > self._STATE_UPDATE_INTERVAL_S:
+                            self.send_update()
+                except Exception as ex:
+                    self._mark_disconnected(ex)
+            elif time.time() - self._last_connect_attempt >= self._MQTT_RECONNECT_INTERVAL_S:
+                self._connect()
+
             self._set_stalled(self._servo.stalled)
 
             self._servo.tick()
-
-            if time.time() - self.last_update > self._STATE_UPDATE_INTERVAL_S:
-                self.send_update()
 
             idle = 0.1 if self._servo.running else 0.5
             await asyncio.sleep(idle)
